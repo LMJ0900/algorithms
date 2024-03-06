f555b160ea50d866cd0859cc14f4d011da32f407
LMJ0900 <alswodnjswo@naver.com>
2024-03-06 19:54:15 +0900

    240306

--- algorism/Ggul.java
+++ algorism/Ggul.java
@@ -9,8 +9,12 @@ public class Ggul {
         int b=0;
         int c = sc.nextInt();
         int d = 0;
+        int e = 0;
+        int f=0;
         int[] arr = new int[c];
         int[] arr1 = new int[c];
+        int[] arr2 = new int[c];
+        int[] arr3 = new int[c];
         for(int i=0; i<arr.length; i++){
             arr[i]= sc.nextInt();
         }
@@ -31,11 +35,45 @@ public class Ggul {
 
         int max = arr1[0];
         for(int i=1; i<arr1.length; i++){
-            if (max < arr[i]){
+            if (max < arr1[i]){
                 max=arr1[i];}
         }
 
-        System.out.println(max);
+
+       d=a-arr[c-1]+arr[0];
+
+        for(int i=2; i<arr.length; i++){
+            for(int j=i+1; j<=arr.length; j++){
+                e=e+arr[c-j];
+            }arr2[i-2]=d+e-arr[c-i];
+            e=0;
+        }
+
+        int max1 = arr2[0];
+        for(int i=1; i<arr2.length; i++){
+            if (max1 < arr2[i]){
+                max1=arr2[i];}
+        }
+        f=a-arr[c-1];
+        for(int i=1; i<arr.length-1; i++){
+            arr3[i-1]=f+arr[i];
+        }
+        int max2 = arr3[0];
+        for(int i=1; i<arr3.length; i++){
+            if (max2 < arr3[i]){
+                max2=arr3[i];}
+        }
+
+
+        if(max>max1){
+            max1=max;
+        } if(max1>max2){
+            max2=max1;
+        }
+
+
+        System.out.println(max2);
+
     }
 
     }
